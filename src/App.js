74df16fce5301f38fa7e5065b1dee260dc22c423
renubind <bindrenu98@gmail.com>
2022-05-07 09:01:23 +0530

    version1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Price from "./component/Pricecal";
+function App() {
+  return (
+    <div className="App">
+  <h1>Stock Profile & <br></br>Loss Calculator</h1>
+  <Price/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Pricecal.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+export default function Pricecal() {
+  const [initial, setInitial] = useState(undefined);
+  const [quantity, setQuantity] = useState(undefined);
+  const [current, setCurrent] = useState(undefined);
+  const [total, setTotal] = useState(undefined);
+
+  const calculatePrice = (e) => {
+    e.preventDefault();
+    const total = (current - initial) * quantity;
+    setTotal(total);
+  };
+
+  return (
+    <div>
+      <div>
+        <h3>Initial Price</h3>
+        <input
+          placeholder="Enter Initial Price Here"
+          value={initial}
+          onChange={(e) => setInitial(e.target.value)}
+        />
+
+        <h3>Qauntity of Price</h3>
+        <input
+          placeholder="Enter the Number of Stocks Bought"
+          value={quantity}
+          onChange={(e) => setQuantity(e.target.value)}
+        />
+
+        <h3>Current Price</h3>
+        <input
+          placeholder="Enter the Current Price of the Stocks"
+          value={current}
+          onChange={(e) => setCurrent(e.target.value)}
+        />
+        <br></br>
+        <button onClick={calculatePrice}>Tell Me!!</button>
+      </div>
+
+        {total>=0 && (<h1>Profile is:{total}</h1>) }
+        {total<0 && (<h1>Loss is:{total}</h1>) }
+
+
+
+      <div>
+        <p>©| 2022 | adarshbalika</p>
+      </div>
+      <div>
+        <img src=".../component/twitter.png" alt="image" />
+        <img src=".../instagram.png" alt="image1" />
+        <img src=".../github.png" alt="image2" />
+      </div>
+    </div>
+  );
+}
